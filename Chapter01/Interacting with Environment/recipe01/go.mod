b67b0522570e4ba69daeb72e3691d494e50caefa
lecs <33053602+larsve@users.noreply.github.com>
2021-10-11 21:47:21 +0200

    Extend log output with module information from runtime/debug

---
+++ Chapter01/Interacting with Environment/recipe01/go.mod
@@ -0,0 +1,5 @@
+module r01
+
+go 1.16
+
+require github.com/rs/zerolog v1.25.0 // indirect
--- Chapter01/Interacting with Environment/recipe01/main.go
+++ Chapter01/Interacting with Environment/recipe01/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"log"
 	"runtime"
+	"runtime/debug"
+
+	"github.com/rs/zerolog/log" // Suck in a new logger, just to have a dependency to log..
 )
 
 const info = `
 Application %s starting.
 The binary was build by GO: %s`
 
+func dumpModule(m *debug.Module, indent ...string) {
+	if m == nil {
+		return
+	}
+	var pf string
+	if len(indent) > 0 {
+		pf = indent[0]
+	}
+	log.Printf("  %sPath: %s", pf, m.Path)
+	log.Printf("  %sVersion: %s", pf, m.Version)
+	log.Printf("  %sChecksum: %s", pf, m.Sum)
+	dumpModule(m.Replace, pf+"  ")
+}
+
 func main() {
 	log.Printf(info, "Example", runtime.Version())
 
+	// Add ReadBuildInfo (added in GO 1.12) to log output
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Print("Failed to read BuildInfo")
+	}
+	log.Printf("Main package path: %s", bi.Path)
+	log.Print("Main module:")
+	dumpModule(&bi.Main)
+	for i, dep := range bi.Deps {
+		log.Printf("Dependency #%d", i)
+		dumpModule(dep)
+	}
 }
